c4675a3e94d343d1b2dda24005dc0a996e1beb65
1v9 <mkb@live.cn>
2019-07-16 06:53:08 +0800

    🐱‍👤

---
+++ index.js
@@ -0,0 +1,35 @@
+/* global hexo */
+
+'use strict';
+
+const path = require("path");
+const cheerio = require("cheerio");
+
+hexo.extend.filter.register('after_post_render', data => {
+  const theme = hexo.theme.config;
+  // Exit if `anchor` option disable in NexT.
+  if (!theme.anchor.enable) return;
+
+  const $ = cheerio.load(data.content, { decodeEntities: false });
+
+  let headings = $('h1, h2, h3, h4, h5, h6');
+  if (!headings.length) return data;
+
+  const position = theme.anchor.position;
+
+  headings.each((x, y) => {
+    let id = $(y).attr('id');
+    let p = $(y).addClass('heading-control');
+    if (position === 'left') {
+      p.prepend('<a class="heading-anchor" href="#' + id + '" aria-hidden="true"></a>');
+    } else if (position === 'right') {
+      p.append('<a class="heading-anchor" href="#' + id + '" aria-hidden="true"></a>');
+    } 
+  });
+
+  data.content = $.html();
+});
+
+hexo.extend.filter.register('theme_inject', injects => {
+  injects.style.push(path.join(__dirname, 'anchor.styl'));
+});
